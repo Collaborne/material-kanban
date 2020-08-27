0c9a5d5cd6ade474f3d40af04caee371b740a5cc
Andreas Kohn <andreas.kohn@gmail.com>
2020-08-27 14:14:43 +0200

    Show how to provide translations in a 'i18n' story

--- .storybook/stories/0-Kanban.stories.tsx
+++ .storybook/stories/0-Kanban.stories.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 
-import { Board, Column } from '../../src';
+import { Board, Card, Column, Intl } from '../../src';
 
 export default {
 	title: 'Kanban',
@@ -15,8 +15,8 @@ const COLUMNS: Column[] = [
 	{ id: 'column2', name: 'Column 2', cards: [] },
 ];
 
-export const Basic = () => {
-	const [columns, setColumns] = useState(COLUMNS);
+function useColumns(initialColumns: Column[]): [Column[], React.Dispatch<React.SetStateAction<Column[]>>, () => Promise<Column>, (column: Column) => Promise<Card>] {
+	const [columns, setColumns] = useState(initialColumns);
 
 	async function createColumn() {
 		const index = columns.length + 1;
@@ -26,6 +26,12 @@ export const Basic = () => {
 	async function createCard(column: Column) {
 		return { id: `card_${column.id}_${column.cards.length + 1}`};
 	}
+
+	return [columns, setColumns, createColumn, createCard];
+}
+
+export const Basic = () => {
+	const [columns, setColumns, createColumn, createCard] = useColumns(COLUMNS);
 
 	return (
 		<Board
@@ -42,3 +48,29 @@ export const Basic = () => {
 		</Board>
 	);
 }
+
+export const I18n = () => {
+	const [columns, setColumns, createColumn, createCard] = useColumns(COLUMNS);
+
+	const intl: Intl = {
+		addCardButtonLabel: 'Neue Karte',
+		addColumnButtonLabel: 'Neue Spalte',
+		columnNamePlaceholder: 'Namenlos',
+	};
+	return (
+		<Board
+			columns={columns}
+			onChange={setColumns}
+			createColumn={createColumn}
+			createCard={createCard}
+			intl={intl}
+		>
+			{card => (
+				<Typography variant="body1" component="div">
+					{card.id}
+				</Typography>
+			)}
+		</Board>
+	);
+}
+I18n.storyName = 'I18n';
