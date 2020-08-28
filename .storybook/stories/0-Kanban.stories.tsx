7d69bee8f202159b20d9ddf1eafd020001c33cf6
Andreas Kohn <andreas.kohn@gmail.com>
2020-08-28 16:17:16 +0200

    Show actions when moving columns and cards in the basic example

--- .storybook/stories/0-Kanban.stories.tsx
+++ .storybook/stories/0-Kanban.stories.tsx
@@ -44,12 +44,23 @@ function SimpleCard<TCard extends Card = Card>({ card, children }: { card: TCard
 export const Basic = () => {
 	const [columns, setColumns, createColumn, createCard] = useColumns(COLUMNS);
 
+	const columnMoved = action('column moved');
+	function handleColumnMove(column: Column, newIndex: number, oldIndex: number) {
+		columnMoved(`${column.id} from ${oldIndex} to ${newIndex}`, {column});
+	}
+	const cardMoved = action('card moved');
+	function handleCardMove(card: Card, newColumn: Column, newIndex: number, oldColumn: Column, oldIndex: number) {
+		cardMoved(`${card.id} from ${oldColumn.id} position ${oldIndex} to ${newColumn.id} position ${newIndex}`, {card, oldColumn, newColumn});
+	}
+
 	return (
 		<Board
 			columns={columns}
 			onChange={setColumns}
 			createColumn={createColumn}
 			createCard={createCard}
+			onCardMoved={handleCardMove}
+			onColumnMoved={handleColumnMove}
 		>
 			{card => <SimpleCard card={card}/>}
 		</Board>
