f1a1c18b6a80d70645a746359f8ecb512d857e86
Andreas Kohn <andreas.kohn@gmail.com>
2020-09-18 17:11:16 +0200

    Remove `src` from the list of always-linked directories now that we include in package.json's files anyways

--- dev-link.sh
+++ dev-link.sh
@@ -15,7 +15,7 @@ exposed_directory=${PWD}/exposed
 rm -rf "${exposed_directory}" && mkdir -p "${exposed_directory}"
 
 # Link the various pieces we need to make it look like our package
-for f in src $(node -p '(require("./package.json").files || []).join("\n")'); do
+for f in $(node -p '(require("./package.json").files || []).join("\n")'); do
 	ln -s "../${f}" "${exposed_directory}/${f}"
 done
 # Hardlink files such as package.json to prevent smart tools trying to bork things.
