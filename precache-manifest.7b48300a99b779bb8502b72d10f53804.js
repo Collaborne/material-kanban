30b0eff80664928b72cea62f80a38350c58a4f6c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-29 13:55:24 +0000

    Deploy Collaborne/material-kanban to github.com/Collaborne/material-kanban.git:gh-pages

---
+++ precache-manifest.7b48300a99b779bb8502b72d10f53804.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c6d6c269dd200f5cbd9090c5d80233",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.04622145348a1725d6b0.bundle.js"
+  },
+  {
+    "url": "runtime~main.04622145348a1725d6b0.bundle.js"
+  },
+  {
+    "url": "vendors~main.04622145348a1725d6b0.bundle.js"
+  },
+  {
+    "url": "vendors~main.04622145348a1725d6b0.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d78281787905b32870f6d01b11c050c8.js"
+  "precache-manifest.7b48300a99b779bb8502b72d10f53804.js"
 );
 
 self.addEventListener('message', (event) => {
