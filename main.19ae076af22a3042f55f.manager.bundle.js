ed0671314f5ff9294f5ac78a92cec3765b0d6b5d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-06-21 11:36:48 +0000

    Deploy Collaborne/material-kanban to github.com/Collaborne/material-kanban.git:gh-pages

--- main.19ae076af22a3042f55f.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{430:function(module,exports,__webpack_require__){__webpack_require__(431),__webpack_require__(812),__webpack_require__(814),__webpack_require__(815),__webpack_require__(816),module.exports=__webpack_require__(813)},497:function(module,exports){}},[[430,2,3]]]);
